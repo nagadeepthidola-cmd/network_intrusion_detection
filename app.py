a6fed3f5698ad22d9a6301224eec3fad1e295e32
nagadeepthidola-cmd <nagadeepthidola@gmail.com>
2026-09-06 15:27:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask_cors import CORS
 import joblib
 import pandas as pd
 
-app = Flask(__name__)
-CORS(app)
+app = Flask(
+    __name__,
+    template_folder=".",
+    static_folder=".",
+    static_url_path=""
+)
 
 
 # Load model
